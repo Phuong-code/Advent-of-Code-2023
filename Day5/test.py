1b3d90dbcded79525844faed7635f487796e95c7
Duy Minh Phuong Vu (Phil) <phuongvu28111996@gmail.com>
2023-12-06 18:15:20 +1100

    Delete test

--- Day5/test.py
+++
@@ -1,8 +0,0 @@
-def fahrenheit_to_celsius(f):
-    return (f - 32) * 5 / 9
-
-temperatures_fahrenheit = [32, 64, 100, 212]
-
-temperatures_celsius = map(fahrenheit_to_celsius, temperatures_fahrenheit)
-
-print(temperatures_celsius)
