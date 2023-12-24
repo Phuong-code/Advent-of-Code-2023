5651f279401de91be5c0eec9968e34546bdf9d10
Duy Minh Phuong Vu (Phil) <phuongvu28111996@gmail.com>
2023-12-24 23:47:48 +1100

    Adding day17 part1 still wrong

---
+++ Day17/part1.py
@@ -0,0 +1,69 @@
+def iterate_diagonally(grid):
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+    for s in range(rows + cols - 1):
+        for row in range(s + 1):
+            col = s - row
+            if row < rows and col < cols:
+                yield (row, col)
+
+def is_not_consec(row, column, direction):
+    global seen
+    global row_numbers
+    global column_numbers
+    if direction == "U":
+        if row+2 < row_numbers:
+            if seen[row][column] == (row+1, column) and seen[row+1][column] == (row+2,column):
+                return False
+    elif direction == "D":
+        if 0 <= row-2:
+            if seen[row][column] == (row-1, column) and seen[row-1][column] == (row-2,column):
+                return False
+    elif direction == "L":
+        if column+2 < column_numbers:
+            if seen[row][column] == (row, column+1) and seen[row][column+1] == (row, column+2):
+                return False
+    elif direction == "R":
+        if 0 <= column-2:
+            if seen[row][column] == (row, column-1) and seen[row][column-1] == (row, column-2):
+                return False
+    return True
+
+with open("Day17/input-example.txt", "r") as file:
+    grid = [list(map(int, line.strip())) for line in file if line.strip()]
+
+
+row_numbers = len(grid)
+column_numbers = len(grid[0])
+
+min_dis = [[float('inf') for _ in range(row_numbers)] for _ in range(column_numbers)]
+min_dis[0][0] = 0
+seen = [[(-1,-1) for _ in range(row_numbers)] for _ in range(column_numbers)]
+
+for row, column in iterate_diagonally(grid):
+    if 0 <= row-1: 
+        cur_dis = min_dis[row][column] + grid[row-1][column]
+        if cur_dis < min_dis[row-1][column] and is_not_consec(row, column, "U"):
+            min_dis[row-1][column] = cur_dis
+            seen[row-1][column] = (row, column)
+    if row+1 < row_numbers:
+        cur_dis = min_dis[row][column] + grid[row+1][column]
+        if cur_dis < min_dis[row+1][column] and is_not_consec(row, column, "D"):
+            min_dis[row+1][column] = cur_dis
+            seen[row+1][column] = (row, column)
+    if 0 <= column-1: 
+        cur_dis = min_dis[row][column] + grid[row][column-1]
+        if cur_dis < min_dis[row][column-1] and is_not_consec(row, column, "L"):
+            min_dis[row][column-1] = cur_dis
+            seen[row][column-1] = (row, column)
+    if column+1 < column_numbers: 
+        cur_dis = min_dis[row][column] + grid[row][column+1]
+        if cur_dis < min_dis[row][column+1] and is_not_consec(row, column, "R"):
+            min_dis[row][column+1] = cur_dis
+            seen[row][column+1] = (row, column)
+
+# Open a file in write mode
+with open('Day17\output_old.txt', 'w') as file:
+    for row in min_dis:
+        # Convert each row to a string and write it to the file
+        file.write(' '.join(map(str, row)) + '\n')
