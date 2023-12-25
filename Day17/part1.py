e1790ab08f2a0aa655a27b21f26300e94b768c1f
Duy Minh Phuong Vu (Phil) <phuongvu28111996@gmail.com>
2023-12-25 23:31:50 +1100

    Adding day17 part1 still wrong

--- Day17/part1.py
+++ Day17/part1.py
@@ -15,18 +15,30 @@ def is_not_consec(row, column, direction):
         if row+2 < row_numbers:
             if seen[row][column] == (row+1, column) and seen[row+1][column] == (row+2,column):
                 return False
+        # if row+3 < row_numbers:
+        #     if seen[row][column] == (row+1, column) and seen[row+1][column] == (row+2,column) and seen[row+2][column] == (row+3,column):
+        #         return False
     elif direction == "D":
-        if 0 <= row-2:
-            if seen[row][column] == (row-1, column) and seen[row-1][column] == (row-2,column):
+        if 0 <= row-3:
+            if seen[row][column] == (row-1, column) and seen[row-1][column] == (row-2,column) and seen[row-2][column] == (row-3,column):
                 return False
+        # if 0 <= row-2:
+        #     if seen[row][column] == (row-1, column) and seen[row-1][column] == (row-2,column):
+        #         return False
     elif direction == "L":
         if column+2 < column_numbers:
             if seen[row][column] == (row, column+1) and seen[row][column+1] == (row, column+2):
                 return False
+        # if column+3 < column_numbers:
+        #     if seen[row][column] == (row, column+1) and seen[row][column+1] == (row, column+2) and seen[row][column+2] == (row, column+3):
+        #         return False
     elif direction == "R":
         if 0 <= column-2:
             if seen[row][column] == (row, column-1) and seen[row][column-1] == (row, column-2):
                 return False
+        # if 0 <= column-3:
+        #     if seen[row][column] == (row, column-1) and seen[row][column-1] == (row, column-2) and seen[row][column-2] == (row, column-3):
+        #         return False
     return True
 
 with open("Day17/input-example.txt", "r") as file:
@@ -40,30 +52,35 @@ min_dis = [[float('inf') for _ in range(row_numbers)] for _ in range(column_numb
 min_dis[0][0] = 0
 seen = [[(-1,-1) for _ in range(row_numbers)] for _ in range(column_numbers)]
 
-for row, column in iterate_diagonally(grid):
-    if 0 <= row-1: 
-        cur_dis = min_dis[row][column] + grid[row-1][column]
-        if cur_dis < min_dis[row-1][column] and is_not_consec(row, column, "U"):
-            min_dis[row-1][column] = cur_dis
-            seen[row-1][column] = (row, column)
-    if row+1 < row_numbers:
-        cur_dis = min_dis[row][column] + grid[row+1][column]
-        if cur_dis < min_dis[row+1][column] and is_not_consec(row, column, "D"):
-            min_dis[row+1][column] = cur_dis
-            seen[row+1][column] = (row, column)
-    if 0 <= column-1: 
-        cur_dis = min_dis[row][column] + grid[row][column-1]
-        if cur_dis < min_dis[row][column-1] and is_not_consec(row, column, "L"):
-            min_dis[row][column-1] = cur_dis
-            seen[row][column-1] = (row, column)
-    if column+1 < column_numbers: 
-        cur_dis = min_dis[row][column] + grid[row][column+1]
-        if cur_dis < min_dis[row][column+1] and is_not_consec(row, column, "R"):
-            min_dis[row][column+1] = cur_dis
-            seen[row][column+1] = (row, column)
+for time in range(10):
+    # for row, column in iterate_diagonally(grid):
+    for row in range(row_numbers):
+        for column in range(column_numbers):
+            if 0 <= row-1: 
+                cur_dis = min_dis[row][column] + grid[row-1][column]
+                if cur_dis < min_dis[row-1][column] and is_not_consec(row, column, "U"):
+                    min_dis[row-1][column] = cur_dis
+                    seen[row-1][column] = (row, column)
+            if row+1 < row_numbers:
+                cur_dis = min_dis[row][column] + grid[row+1][column]
+                if cur_dis < min_dis[row+1][column] and is_not_consec(row, column, "D"):
+                    min_dis[row+1][column] = cur_dis
+                    seen[row+1][column] = (row, column)
+            if 0 <= column-1: 
+                cur_dis = min_dis[row][column] + grid[row][column-1]
+                if cur_dis < min_dis[row][column-1] and is_not_consec(row, column, "L"):
+                    min_dis[row][column-1] = cur_dis
+                    seen[row][column-1] = (row, column)
+            if column+1 < column_numbers: 
+                cur_dis = min_dis[row][column] + grid[row][column+1]
+                if cur_dis < min_dis[row][column+1] and is_not_consec(row, column, "R"):
+                    min_dis[row][column+1] = cur_dis
+                    seen[row][column+1] = (row, column)
 
 # Open a file in write mode
 with open('Day17\output_old.txt', 'w') as file:
-    for row in min_dis:
+    for row in seen:
         # Convert each row to a string and write it to the file
         file.write(' '.join(map(str, row)) + '\n')
+
+#962 too high
