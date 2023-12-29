0120e27d3addae7db45a2a71662af2b1d156bc4e
Duy Minh Phuong Vu (Phil) <phuongvu28111996@gmail.com>
2023-12-30 00:14:49 +1100

    Adding day21 part1

--- Day20/test.py
+++
@@ -1,3 +0,0 @@
-dict = {1:4 , 2:1}
-result = all(value > 0 for value in dict.values())
-print(result)
---
+++ Day21/part2.py
@@ -0,0 +1,33 @@
+with open("Day21\input-example.txt", "r") as file:
+    grid = [list(line) for line in file.read().split("\n")]
+
+row_num = len(grid)
+col_num = len(grid[0])
+for row in range(row_num):
+    for col in range(col_num):
+        if grid[row][col] == "S":
+            s_row = row
+            s_col = col
+
+seen = set((s_row, s_col))
+ans = set()
+queue = [(s_row, s_col, 0)]
+
+while queue:
+    row, col, step = queue.pop(0)
+    if step % 2 == 0:
+        ans.add((row, col))
+    if step == 265013:
+        continue
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        new_row = row + dr
+        new_col = col + dc
+        if (0 <= new_row < row_num and 
+            0 <= new_col < col_num and 
+            grid[new_row][new_col] != "#" and
+            (new_row, new_col) not in seen):
+            seen.add((new_row, new_col))
+            queue.append((new_row, new_col, step+1))
+
+# print(ans)
+print(len(ans))
